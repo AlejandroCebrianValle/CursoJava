a60ab7810d7f5e8ea87086d6f9b2d4a784b24e0b
AlejandroCebrianValle <16271236+AlejandroCebrianValle@users.noreply.github.com>
2019-09-25 12:56:54 +0200

    Add files via upload

--- ProgramasInicio/Enunciados25Septiembre.java
+++ ProgramasInicio/Enunciados25Septiembre.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Enunciados25Septiembre {
 
 	public static void main(String[] args) {
+		// Variables generales
 		// Variables Primer Ejercicio
 		int i;
 		double longi;
@@ -25,6 +26,7 @@ public class Enunciados25Septiembre {
 		double numero;
 		double suma=0;
 		double media=0;
+		//  Main //
 		while (true){
 			System.out.println("Ejercicio 1");
 			for (i=15; i<=180; i+=15) {
@@ -56,17 +58,17 @@ public class Enunciados25Septiembre {
 			System.out.println("Tercer lado: ");
 			c = in.nextDouble();
 			if (a == b && a == c && b == c) {
-				System.out.println("Su triÃ¡ngulo es EquilÃ¡tero.");
+				System.out.println("Su triángulo es Equilátero.");
 			}
 			else if (a == b || a == c || b == c) {
-				System.out.println("Su triÃ¡ngulo es IsÃ³sceles.");
+				System.out.println("Su triángulo es Isósceles.");
 			}
 			else {
-				System.out.println("Su triÃ¡ngulo es Escaleno.");
+				System.out.println("Su triángulo es Escaleno.");
 			}
 			System.out.println("--------------------------------------------"
 					+ "\nEjercicio 5");
-			System.out.println("Ingrese el nÃºmero a calcular su factorial:");
+			System.out.println("Ingrese el número a calcular su factorial:");
 			factor = in.nextInt();
 			
 			for (i=1; i <= factor; i++) {
@@ -77,14 +79,12 @@ public class Enunciados25Septiembre {
 			System.out.println("--------------------------------------------"
 					+ "\nEjercicio 6");
 			for (m=1; m<=10; m++) {
-				System.out.println("Introduzca el " + m +"Âº numero:");
+				System.out.println("Introduzca el " + m +"º numero:");
 				numero = in.nextDouble();
 				suma+=numero;
 				media+=(numero/10);
-				System.out.println("La media de los nÃºmero introducidos es:"
-						+ media +"\nEl sumatorio es: " + suma);
 			}
-			System.out.println("La media de los nÃºmero introducidos es: "
+			System.out.println("La media de los número introducidos es: "
 								+ media +"\nEl sumatorio es: " + suma);
 			in.close();
 			break;
